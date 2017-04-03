a14a60085ac69a7d058e9c9001fa04333d9c9ff4
cory <wolfe.cory1@gmail.com>
2017-04-03 14:22:29 -0500

    SD and improved functionality

--- graphicstest.ino
+++ graphicstest.ino
@@ -1,8 +1,4 @@
-// This sketch has been Refurbished by BUHOSOFT
-// IMPORTANT: Adafruit_TFTLCD LIBRARY MUST BE SPECIFICALLY
-// CONFIGURED FOR EITHER THE TFT SHIELD OR THE BREAKOUT BOARD.
-// SEE RELEVANT COMMENTS IN Adafruit_TFTLCD.h FOR SETUP.
-//#define DEBUG
+
 #include <Adafruit_GFX.h>     // Core graphics library
 #include <Adafruit_TFTLCD.h>  // Hardware-specific library
 #include <SPI.h>              // SPI general
@@ -18,18 +14,6 @@
 
 #define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
 
-// When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
-// For the Arduino Uno, Duemilanove, Diecimila, etc.:
-//   D0 connects to digital pin 8  (Notice these are
-//   D1 connects to digital pin 9   NOT in order!)
-//   D2 connects to digital pin 2
-//   D3 connects to digital pin 3
-//   D4 connects to digital pin 4
-//   D5 connects to digital pin 5
-//   D6 connects to digital pin 6
-//   D7 connects to digital pin 7
-// For the Arduino Mega, use digital pins 22 through 29
-// (on the 2-row header at the end of the board).
 
 // Assign human-readable names to some common 16-bit color values:
 #define	BLACK   0x0000
@@ -45,188 +29,71 @@
   const int hall = A5;
   const int chipSelect = 4;
   File dataFile;
-  int preState=0;
+  int preState = 0;
   int rev = 0;
   int intTime;
   int rots = 0;
   long avgSpd = 0;
   long rotSpd = 0;
   long dist =0;
-  long diameter = 2.2507;//0.000426261; //in miles, so already in miles/(time)
+  long diameter = 2.2507;
   int splits[] = {0,0,0,0,0};
   int sDist[] = {0,0,0,0,0};
   int s = 4;
   long rpm = 0;
+  long shtSpd = 0;
+  long lstUpdate;
+  int x,y,w;
+  int curState;
+  long curTime;
+  
 
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
-// If using the shield, all control and data lines are fixed, and
-// a simpler declaration can optionally be used:
-// Adafruit_TFTLCD tft;
-
-//void setup(void) {
 void setup(){
   pinMode(SS, OUTPUT);
-
- #ifdef DEBUG
-  Serial.begin(9600);
-  Serial.println(F("TFT LCD test"));
-  Serial.print("TFT size is ");
-  Serial.print(tft.width());
-  Serial.print("x");
-  Serial.println(tft.height());
-#endif // DEBUG
   tft.reset();
-
   uint16_t identifier = tft.readID();
-
-if(identifier == 0x9325) {
-#ifdef DEBUG
-    Serial.println(F("Found ILI9325 LCD driver"));
-  } else if(identifier == 0x9328) {
-
-    Serial.println(F("Found ILI9328 LCD driver"));
-  } else if(identifier == 0x7575) {
-
-    Serial.println(F("Found HX8347G LCD driver"));
-  } else if(identifier == 0x9341) {
-
-    Serial.println(F("Found ILI9341 LCD driver"));
-  } else if(identifier == 0x8357) {
-
-    Serial.println(F("Found HX8357D LCD driver"));
-#endif // DEBUG
-    } else {
-    #ifdef DEBUG
-    Serial.print(F("Unknown LCD driver chip: "));
-    Serial.println(identifier, HEX);
-    Serial.print(F("I try use ILI9341 LCD driver "));
-    Serial.println(F("If using the Adafruit 2.8\" TFT Arduino shield, the line:"));
-    Serial.println(F("  #define USE_ADAFRUIT_SHIELD_PINOUT"));
-    Serial.println(F("should appear in the library header (Adafruit_TFT.h)."));
-    Serial.println(F("If using the breakout board, it should NOT be #defined!"));
-    Serial.println(F("Also if using the breakout, double-check that all wiring"));
-    Serial.println(F("matches the tutorial."));
-    #endif // DEBUG
-    identifier = 0x9341;
-  }
-
-
   tft.begin(identifier);
-#ifdef DEBUG
-  Serial.println(F("Benchmark                Time (microseconds)"));
-
-  Serial.print(F("Screen fill              "));
-  Serial.println(testFillScreen());
-  delay(500);
-
-  Serial.print(F("Text                     "));
-  Serial.println(testText());
-  delay(3000);
-
-  Serial.print(F("Lines                    "));
-  Serial.println(testLines(CYAN));
-  delay(500);
-
-  Serial.print(F("Horiz/Vert Lines         "));
-  Serial.println(testFastLines(RED, BLUE));
-  delay(500);
-
-  Serial.print(F("Rectangles (outline)     "));
-  Serial.println(testRects(GREEN));
-  delay(500);
-
-  Serial.print(F("Rectangles (filled)      "));
-  Serial.println(testFilledRects(YELLOW, MAGENTA));
-  delay(500);
-
-  Serial.print(F("Circles (filled)         "));
-  Serial.println(testFilledCircles(10, MAGENTA));
-
-  Serial.print(F("Circles (outline)        "));
-  Serial.println(testCircles(10, WHITE));
-  delay(500);
-
-  Serial.print(F("Triangles (outline)      "));
-  Serial.println(testTriangles());
-  delay(500);
-
-  Serial.print(F("Triangles (filled)       "));
-  Serial.println(testFilledTriangles());
-  delay(500);
-
-  Serial.print(F("Rounded rects (outline)  "));
-  Serial.println(testRoundRects());
-  delay(500);
-
-  Serial.print(F("Rounded rects (filled)   "));
-  Serial.println(testFilledRoundRects());
-  delay(500);
-
-  Serial.println(F("Done!"));
-#endif // DEBUG
   tft.setRotation(0);
+  //Begin SD Testing Block
   if (!SD.begin(chipSelect)) {
     errorSD();
     while (1) ;
   }
-  Serial.println("card initialized.");
   dataFile = SD.open("datalog.txt", FILE_WRITE);
   if (! dataFile) {
     errorFile();
-    // Wait forever since we cant write data
     while (1) ;
   }
+  //End SD Testing Block
+  //Initialized Vars
   intTime = millis();
+  lstUpdate = intTime;
+  int x,y,w = tft.width(),h = tft.height();
+  tft.setTextColor(WHITE); tft.setTextSize(3);
+  tft.fillScreen(BLACK);
+  preState = 0;
+  curTime = intTime;
+  String header = "Time,On,Distance,RPM,Speed";
+  dataFile.println(header);
+  dataFile.flush();
 }
 
 void loop(void) {
-  //printDebug();
   printRun();
-  String dataString="";
-  int sensor = analogRead(hall);
-  dataString+=String(millis()-intTime);
-  dataString+=String(",");
-  dataString+=String(sensor);
-  dataFile.println(dataString);
-  Serial.println(dataString);
-  dataFile.flush();
-  //delay(50);
-  /*for(uint8_t rotation=0; rotation<4; rotation++) {
-    tft.setRotation(rotation);
-    testText();
-    delay(2000);
+  if(curState == 1){
+  
   }
-  testFilledRoundRects();
-  testRoundRects();
-  testFilledTriangles();
-  testTriangles();
-  testCircles(10, WHITE);
-  testFillScreen();
-  testText();
-  testLines(CYAN);
-  testFastLines(RED, BLUE);
-  testRects(GREEN);
-  testFilledRects(YELLOW, MAGENTA);
-  testFilledCircles(10, MAGENTA);*/
 }
 
-unsigned long testFillScreen() {
-  unsigned long start = micros();
-  tft.fillScreen(BLACK);
-  tft.fillScreen(RED);
-  tft.fillScreen(GREEN);
-  tft.fillScreen(BLUE);
-  tft.fillScreen(BLACK);
-  return micros() - start;
-}
-unsigned long errorSD(){
+unsigned long errorSD(){//If there is no SD card, halt and request one.
  tft.fillScreen(BLACK);
   unsigned long start = micros();
   tft.setCursor(0, 0);
   tft.setTextColor(WHITE);  tft.setTextSize(1);
   tft.println("Insert SD card and restart.");
 }
-unsigned long errorFile(){
+unsigned long errorFile(){ //If there is a problem with the SD card, halt and request a different one.
  tft.fillScreen(BLACK);
   unsigned long start = micros();
   tft.setCursor(0, 0);
@@ -234,85 +101,74 @@ unsigned long errorFile(){
   tft.println("We can't open the file, try a different SD card.");
 }
 
-unsigned long printDebug(){
-  int curState = analogRead(A5);
-  //tft.fillScreen(BLACK);
-  int x,y,w = tft.width(),h = tft.height();
-  for(y=24;y<47;y++) tft.drawFastHLine(0,y,w,BLACK);
-  for(x=0;x<w;x++) tft.drawFastHLine(x/4,0,h/4,BLACK);
-  for(y=74;y<102;y++) tft.drawFastHLine(0,y,w,BLACK);
-  for(x=0;x<w;x++) tft.drawFastHLine(x,0,h/4,BLACK);
-  unsigned long start = micros();
-  tft.setCursor(0,0);
-  tft.setTextColor(WHITE); tft.setTextSize(3);
-  tft.print("Pin A5 reads: ");
-  tft.println(curState);
-  //tft.println("");
-  tft.print("Is triggered? ");
-  if(curState<15)
-  tft.println("yes");else
-  tft.println("no");
-  /*Following code block is for total number of increments
-  if(curState!=preState && curState == 0){
-  rev++;
-  }
-  tft.println("Number of revolutions: " + rev);
-  preState = curState;
-  //end*/
-  return micros() - start;
-}
-
-unsigned long printRun(){
-  int curState = analogRead(A5);
-  if(curState<15)
+unsigned long printRun(){ //Function to run under normal operation
+  int sens = analogRead(A5);
+  curTime = millis();
+  if(sens<15)
     curState = 1;
   else
     curState = 0;
-  if(curState==1){
-  unsigned long curTime = millis();
-  if(curState==1 && curState!=preState){
-    rots++;
+  if(curState==1){//See if sensor is triggered.
+  if(curState!=preState){//Make sure the sensor wasn't previously triggered.
+  rots++;
+  dist = diameter*3.14159265*rots;//Calculate distance from diameter*pi*(# of rotations)
+  avgSpd = dist/(((curTime-intTime)*.001)/360);//Find total avg speed from distance over time in hours.
+  //The following block records the last five sets to memory speed calcs.
+  for(int i = 0;i<=3;i++){
+    splits[i] = splits[i+1]; //splits is time (ms)
+    sDist[i] = sDist[i+1];  //sDist is distance (ft)
   }
-  dist = diameter*3.14159265*rots;
-  avgSpd = dist/(((curTime-intTime)*.001)/360);
-  splits[0] =  splits[1];
-  sDist[0] = sDist[1];
-  splits[1] =  splits[2];
-  sDist[1] = sDist[2];
-  splits[2] =  splits[3];
-  sDist[2] = sDist[3];
-  splits[3] =  splits[4];
-  sDist[3] = sDist[4];
   splits[4] = curTime;
   sDist[4]=dist;
-
-  rpm = ((sDist[4]-sDist[1])/(diameter*3.14159265))/(.001/60*(splits[4]-splits[1]));
-  long shtSpd = .681818*((sDist[4]-sDist[1])/(.001*(splits[4]-splits[1])));
-  if((splits[4]-splits[3])>250){
+  //END PREVIOUS BLOCK
+  //Short means based on last 5 rotations.
+  rpm = ((sDist[4]-sDist[1])/(diameter*3.14159265))/(.001/60*(splits[4]-splits[1])); //Find short RPM (based on last 5 revs).
+  long shtSpd = .681818*((sDist[4]-sDist[1])/(.001*(splits[4]-splits[1]))); //Find short speed, convert to mph.
+  long tDelta = curTime-lstUpdate; // Calculate time between the last time the screen updated and now
+  /*
+   * Writes to screen at most every 350 ms
+   * This allows for more data to be gathered
+   * This is due to a limitation of the screen's
+   * refresh rate.
+   * 
+   * This code only writes to the screen.
+   * The data is still calculated and saved, even if
+   * the screen is not written to.
+   */
+  if(tDelta>350){ 
   int x,y,w = tft.width(),h = tft.height();
+  //The for loops clear out parts of text that change.
   for(y=24;y<46;y++) tft.drawFastHLine(0,y,w,BLACK);
   for(x=0;x<w/4;x++) tft.drawFastHLine(x/4,0,h/4,BLACK);
   for(y=71;y<94;y++) tft.drawFastHLine(0,y,w,BLACK);
   for(x=0;x<w/4;x++) tft.drawFastHLine(x,0,h/4,BLACK);
-  for(y=120;y<140;y++) tft.drawFastHLine(0,y,w,BLACK);
+  for(y=120;y<146;y++) tft.drawFastHLine(0,y,w,BLACK);
   for(x=0;x<w/4;x++) tft.drawFastHLine(x,0,h/4,BLACK);
   tft.setCursor(0,0);
-  tft.setTextColor(WHITE); tft.setTextSize(3);
-  //tft.println("");
-
+  //Prints distance (ft.), RPM (rpm) and Speed (mph)
   tft.println("Distance: ");
-  tft.println(dist);
-  
+  tft.print(dist); tft.println(" ft.");
   tft.println("Avg. RPM: ");
   tft.println(rpm);
-  
   tft.println("Speed: ");
-  if(splits[0]!=0)
-  tft.println(shtSpd);
+  if(splits[0]!=0){
+  tft.print(shtSpd); tft.println(" mph");}
+  lstUpdate = curTime; //Records the time the screen was updated for future use.
+  }}
+  String dataString = "";
+  dataString+=String(curTime-intTime);
+  dataString+=String(",");
+  dataString+=String(curState);
+  dataString+=String(",");
+  dataString+=String(dist);
+  dataString+=String(",");
+  dataString+=String(rpm);
+  dataString+=String(",");
+  dataString+=String(shtSpd);
+  dataFile.println(dataString);
+  Serial.println(dataString);
+  dataFile.flush();
   }
-  
-  }
-  int preState = curState;
+  preState = curState; //Records current state for future use.
 }
-
 
